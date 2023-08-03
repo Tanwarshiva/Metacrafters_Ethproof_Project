47f509898fd6d51116beb9a30c196397d36a6a83
ShiVa <64605016+Tanwarshiva@users.noreply.github.com>
2023-08-03 17:06:08 +0530

    Create tokening.sol

---
+++ tokening.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.18;
+
+contract MyToken {
+    // Variables
+    string public tokenName = "SHIVA";
+    string public tokenSymbol = "Hi";
+    uint256 public totalSupply = 0;
+
+    // Mapping
+    mapping(address => uint256) public balances;
+
+    // Mint Function
+    function mint(address _to, uint256 _value) external {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn Function
+    function burn(uint256 _value) external {
+        totalSupply -= _value;
+        balances[msg.sender] -= _value;
+    }
+}
